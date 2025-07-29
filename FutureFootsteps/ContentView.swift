6e9b99b6e926d2724e6cbbf874f280b9fce5c0df
Shree Reddi <shreelakshya.reddi@gmail.com>
2025-07-29 13:11:21 -0400

    Initial Commit

---
+++ FutureFootsteps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FutureFootsteps
+//
+//  Created by Shreelakshya Reddi on 7/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FutureFootsteps/FutureFootstepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureFootstepsApp.swift
+//  FutureFootsteps
+//
+//  Created by Shreelakshya Reddi on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FutureFootstepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
