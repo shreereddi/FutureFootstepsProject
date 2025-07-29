d83a1cfeca008fca6354543afc92999fe5406ff5
Shree Reddi <shreelakshya.reddi@gmail.com>
2025-07-29 16:02:00 -0400

    Added the To-Do List functionality

--- FutureFootsteps/ContentView.swift
+++ FutureFootsteps/ContentView.swift
@@ -6,21 +6,60 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Change 1")
-            Text("Change 2")
+        NavigationStack{
+            VStack{
+                HStack{
+                    Text("Sustainability Checklist")
+                        .font(.system(size: 40))
+                        .fontWeight(.black)
+                    Spacer()
+                    NavigationLink(destination: NewToDoView(showNewTask: .constant(false), toDoItem: ToDoItem(title: "", isImportant: false))) {
+                        Text("+")
+                        
+                    }
+                    .font(.system(size: 50))
+                }
+                .padding()
+                Spacer()
+                
+                
+                List{
+                    ForEach(toDos){ toDoItem in
+                        if toDoItem.isImportant {
+                            Text("‼️" + toDoItem.title)
+                        } else {
+                            Text(toDoItem.title)
+                        }
+                    }
+                    .onDelete(perform: deleteToDo)
+                }
+                .listStyle(.plain)
+                
+                
+            }
+            if showNewTask {
+                NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+            }
+            Spacer()
         }
-        .padding()
-    }
+        }
+        func deleteToDo(at offsets: IndexSet) {
+            for offset in offsets {
+                let toDoItem = toDos[offset]
+                modelContext.delete(toDoItem)
+            }
+        }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
+
 }
