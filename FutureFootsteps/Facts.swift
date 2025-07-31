1a1c8b6085ff1891f7f7e7044564a635cf2da61f
Shree Reddi <shreelakshya.reddi@gmail.com>
2025-07-31 16:31:02 -0400

    Added the Facts sections

--- FutureFootsteps/Facts.swift
+++ FutureFootsteps/Facts.swift
@@ -9,28 +9,35 @@ import SwiftUI
 
 struct Facts: View {
     var body: some View {
-        NavigationStack {
-                    VStack(spacing: 30) {
-                        Text("Sustainability")
-                            .font(.largeTitle)
-                            .bold()                        
-                        NavigationLink("Sustainability Facts") {
-                            FactsView()
-                        }
-                        .buttonStyle(.borderedProminent)
-
-                        NavigationLink("Links + Videos") {
-                            LinksView()
-                        }
-                        .buttonStyle(.borderedProminent)
-
-                        NavigationLink("The Future") {
-                            FutureView()
-                        }
-                        .buttonStyle(.borderedProminent)
+            NavigationStack {
+                ZStack{
+                    Color(red: 0.9, green: 0.95, blue: 0.9)
+                        .ignoresSafeArea()
+                VStack(spacing: 30) {
+                    Text("Sustainability")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color(red: 0.406, green: 0.477, blue: 0.261))
+                    NavigationLink("Sustainability Facts") {
+                        FactsView()
                     }
-                    .padding()
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(red: 0.406, green: 0.477, blue: 0.261))
+                    NavigationLink("Links + Videos") {
+                        LinksView()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(red: 0.406, green: 0.477, blue: 0.261))
+                    
+                    NavigationLink("The Future") {
+                        FutureView()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(red: 0.406, green: 0.477, blue: 0.261))
                 }
+                .padding()
+            }
+        }
     }
 }
 
